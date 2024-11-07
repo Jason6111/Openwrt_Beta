4dcedeff2912bd0c21b76ca5908622c5fc6b2eb6
小尨 <43496459+Jason6111@users.noreply.github.com>
2024-11-07 18:17:50 +0800

    Update beta-diy-part2.sh

--- diy_script/beta-diy-part2.sh
+++ diy_script/beta-diy-part2.sh
@@ -54,4 +54,5 @@ sed -i 's/charts\.d = no/charts\.d = yes/g' ./feeds/luci/applications/luci-app-n
 curl -o new_update_cloudflare_com_v4.sh https://raw.githubusercontent.com/Jason6111/Openwrt_Beta/refs/heads/main/patch/update_cloudflare_com_v4.sh
 mv new_update_cloudflare_com_v4.sh ./feeds/packages/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
 #sed -i '1,$d' ./feeds/packages/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh && cat <(curl -s https://raw.githubusercontent.com/Jason6111/Openwrt_Beta/refs/heads/main/patch/update_cloudflare_com_v4.sh) >> ./feeds/packages/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
+sed -i 's|/etc/lucky|/etc/config/lucky2|g' ./feeds/luci/applications/luci-app-lucky/root/etc/config/lucky
 
